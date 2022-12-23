993f0aa461488628655c1937af66651f1c4449ed
João Trindade <joao.pedro.trindade@inmanta.com>
2022-12-23 08:48:51 +0000

    Port srlinux to lsm quickstart (#31)
    * initial commit
    
    * fixed descriptions
    
    * script that copies code to the correct place
    
    * fixed script
    
    * used docstrings for descriptions
    
    * fixed regex
    
    * cleanup imports
    
    * fixed error
    
    * update project.yml
    
    * changed script to modify owner
    
    * add pypi package
    
    * remove topology.yml.bak
    
    * added comanaged = false
    
    * removed pypi package
    
    * put implements on the same line
    
    * removed index
    
    * updated README.md
    
    * revert inclusion of pypi package
    
    * updated code and readme
    
    * fixed link
    
    * minor fixes
    
    * no need for escaped characters
    
    * last fixes

---
+++ lsm-srlinux/setup.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+environment=$(egrep '[0-9a-f]{8}-([0-9a-f]{4}-){3}[0-9a-f]{12}' -o .inmanta)
+dir=/var/lib/inmanta/server/environments/$environment
+
+mkdir $dir
+cp -r /code/* $dir
+
+sudo chown -R inmanta $dir

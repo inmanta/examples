227b8969c77f90928f99ca8cde30af667ebd91df
hugo <hugo.thiolliere@inmanta.com>
2025-01-23 15:18:19 +0100

    wip

--- Networking/SR Linux/ci/do_test_deployment_and_verify.py
+++ Networking/SR Linux/ci/do_test_deployment_and_verify.py
@@ -109,6 +109,7 @@ async def main():
             result = await client.resource_list(tid=environment_id, deploy_summary=True)
             assert result.code == 200
             print(result.result["data"])
+            print(result.result["metadata"])
             return len(result.result["data"]) == n_resources
 
         await retry_limited(done_deploying, timeout=6, interval=1)
